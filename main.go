d1d49457d8387ab4983e1cecbdad6c7d922980cb
RikuNomura0710 <riku_fly_fr1007@yahoo.co.jp>
2023-11-12 03:01:16 +0900

    fix

--- main.go
+++ main.go
@@ -67,7 +67,7 @@ func main() {
     dbPass := os.Getenv("DB_PASSWORD")
     dbHost := os.Getenv("DB_HOST")
     dbName := os.Getenv("DB_NAME")
-    dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
+    dsn := fmt.Sprintf("%s:%s@%s/%s", dbUser, dbPass, dbHost, dbName)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
